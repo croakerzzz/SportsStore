f78cb9e751ac8298bbc81173584d0c78b0c61fc4
Sergey Elshin <elshin@proitr.ru>
2023-07-25 13:13:49 +0500

    расширение функционала cartDetail

--- src/app/store/cartDetail.component.ts
+++ src/app/store/cartDetail.component.ts
@@ -1,8 +1,18 @@
 import {Component} from "@angular/core";
+import {Cart} from "../model/cart.model";
+import {Product} from "../model/product.model";
 
 @Component({
   selector: 'cart-detail',
-  template: '<div><h3 class="bg-info p-a-1">Cart Detail Component</h3></div>'
+  templateUrl: 'cartDetail.component.html'
 })
 export class CartDetailComponent {
+
+  constructor(public cart: Cart) {
+  }
+
+  updateQuantity(product: Product, $event: any) {
+    this.cart.updateQuantity(product, $event.target.value);
+  }
+
 }
