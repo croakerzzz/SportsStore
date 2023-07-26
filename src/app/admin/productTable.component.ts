81ca21aa57de28694cdabf203cfc15197f7499df
Sergey Elshin <elshin@proitr.ru>
2023-07-26 14:07:28 +0500

    таблица с товарами (9.24)

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,7 +1,21 @@
 import {Component} from "@angular/core";
+import {ProductRepository} from "../model/product.repository";
+import {Product} from "../model/product.model";
 
 @Component({
-  template: '<div class="bg-info p-a-1"> <h3>Product Table Placeholder</h3> </div>'
+  templateUrl: 'productTable.component.html'
 })
 export class ProductTableComponent {
+
+  constructor(private repository: ProductRepository) {
+  }
+
+  getProducts(): Product[] {
+    return this.repository.getProducts();
+  }
+
+  deleteProduct(id: number) {
+    this.repository.deleteProduct(id);
+  }
+
 }
